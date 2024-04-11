489c7fda9f48f29dd48a962db2d8a7821239e0c6
jotaBlanco <juanmartinblancoo@gmail.com>
2024-04-11 16:15:12 -0300

    main

--- src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(error, req, res, next) {
-    return res.json({
-        statusCode: error.statusCode || 500,
-        message: error.message || "CODER API ERROR",
-    })
-}
-export default errorHandler
